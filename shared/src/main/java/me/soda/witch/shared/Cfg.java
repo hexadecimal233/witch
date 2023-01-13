45d083da001d06bf11e4268fc62f2acce3f2a7a1
ThebestkillerTBK <2593828650@qq.com>
2023-01-13 21:40:11 +0800

    upd

--- shared/src/main/java/me/soda/witch/shared/Cfg.java
+++ shared/src/main/java/me/soda/witch/shared/Cfg.java
@@ -4,5 +4,4 @@ public class Cfg {
     public static String host = "127.0.0.1";
     public static int port = 11451;
     public static byte[] key = "witched".getBytes();
-    public static String name = "Witch";
 }
