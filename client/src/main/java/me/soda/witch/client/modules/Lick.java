6c48b91f31bd59d7dea86501c66aed6736c594c5
ThebestkillerTBK <2593828650@qq.com>
2023-01-27 10:36:55 +0800

    Update

---
+++ client/src/main/java/me/soda/witch/client/modules/Lick.java
@@ -0,0 +1,50 @@
+package me.soda.witch.client.modules;
+
+import me.soda.witch.client.Witch;
+import me.soda.witch.client.events.TickEvent;
+import me.soda.witch.client.utils.MCUtils;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import static me.soda.witch.client.Witch.mc;
+
+public class Lick {
+    public static final Lick INSTANCE = new Lick();
+
+    float pitch = 0;
+    boolean down = false;
+
+    @EventHandler
+    private void onTick(TickEvent event) {
+        if (!MCUtils.canUpdate()) return;
+        pitch = pitch + (down ? -7 : 7);
+        if (pitch >= 50) {
+            down = true;
+        } else if (pitch <= -50) {
+            down = false;
+        }
+
+        PlayerEntity target = null;
+        for (Entity entity : mc.world.getEntities()) {
+            if (entity instanceof PlayerEntity player && player != mc.player && player.distanceTo(mc.player) < 1.25) {
+                target = player;
+                break;
+            }
+        }
+
+        mc.options.sneakKey.setPressed(target != null);
+        if (target != null) {
+            mc.player.setYaw(MCUtils.relativeYaw(target));
+            mc.player.setPitch(pitch);
+        }
+    }
+
+    public void lick(boolean lick) {
+        pitch = 0;
+        if (lick)
+            Witch.EVENT_BUS.subscribe(this);
+        else
+            Witch.EVENT_BUS.unsubscribe(this);
+    }
+}
